7b9908d5af013a51f11b78bb3981976f312a2bb6
mustakim nagori <mustakimkhan1111176@gmail.com>
2023-06-28 09:46:22 +0530

    Atlas Added

--- database/connection.js
+++ database/connection.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose')
-const { MongoMemoryServer } = require('mongodb-memory-server')
+// const { MongoMemoryServer } = require('mongodb-memory-server')
 require('dotenv').config()
 async function connect() {
-    const mongodb = new MongoMemoryServer()
-    await mongodb.start()
-    const mongo_URI = mongodb.getUri()
+    // const mongodb = new MongoMemoryServer()
+    // await mongodb.start()
+    // const mongo_URI = mongodb.getUri()
     // const db = await mongoose.connect(mongo_URI)
-    const db = process.env.MONGODB_URI
-    console.log("mongodb connected")
+    const uri = process.env.MONGODB_URI
+    mongoose.set('strictQuery', true)
+    const db = await mongoose.connect(uri);
+    console.log("Database Connected")
     return db
 
 
